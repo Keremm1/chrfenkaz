75f1a8edc4b0f38c1ffc8d8fdfa6d7794d8f3382
Keremm1 <113975041+Keremm1@users.noreply.github.com>
2023-02-12 01:29:57 +0300

    Update views.py

--- chrfenkaz/app/views.py
+++ chrfenkaz/app/views.py
@@ -2,6 +2,7 @@ from django.contrib.auth import authenticate, login ,logout
 from django.views.generic import View
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.models import User
+from django.http import HttpResponse
 
 class RegisterView(View):
     def post(self, request):
@@ -9,7 +10,7 @@ class RegisterView(View):
         password = request.POST.get('password')
         user = User.objects.create_user(username=username, password=password, email="default@gmail.com")
         user.save()
-
+        return HttpResponse('Success')
 class LoginView(View):
     def post(self, request):
         username = request.POST.get('username')
@@ -17,7 +18,9 @@ class LoginView(View):
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-
+        return HttpResponse('Success')
+   
 class LogoutView(View,LoginRequiredMixin):
     def post(self, request):
         logout(request)
+        return HttpResponse('Success')
